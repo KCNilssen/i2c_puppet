1dc16beceb4c6a9f337b752c5048dc4351b1a006
KCNilssen <kristian.c.nilssen@gmail.com>
2023-06-27 21:03:07 -0700

    Update pin numbers for OB

--- boards/bbq20kbd_breakout.h
+++ boards/bbq20kbd_breakout.h
@@ -7,46 +7,46 @@
 #define PIN_INT				0
 #define PIN_BKL				25
 
-#define PIN_SDA				18
-#define PIN_SCL				23
+#define PIN_SDA				4	//18
+#define PIN_SCL				2 	//23
 
-#define PIN_TP_RESET		16
-#define PIN_TP_MOTION		22
-#define PIN_TP_SHUTDOWN		24
+#define PIN_TP_RESET		5 	//16
+#define PIN_TP_MOTION		3	//22
+#define PIN_TP_SHUTDOWN		1	//24
 
 #define PIN_PUPPET_SDA		28
 #define PIN_PUPPET_SCL		29
 
 #define NUM_OF_ROWS			7
 #define PINS_ROWS \
-	1, \
-	2, \
-	3, \
-	4, \
-	5, \
-	6, \
-	7
+	24, \
+	23, \
+	22, \
+	19, \
+	18, \
+	14, \
+	13
 
 #define NUM_OF_COLS			6
 #define PINS_COLS \
+	6,  \
+	7,  \
 	8,  \
 	9,  \
-	14, \
-	13, \
-	12, \
+	10, \
 	11
 
 #define NUM_OF_BTNS			1
 #define PINS_BTNS \
-	10,
+	12,
 #define BTN_KEYS \
 	{ KEY_BTN_RIGHT2 },
 
 #define PIN_GPIOEXP0		15
-#define PIN_GPIOEXP1		17
-#define PIN_GPIOEXP2		19
+#define PIN_GPIOEXP1		16
+#define PIN_GPIOEXP2		17
 #define PIN_GPIOEXP3		21
-#define PIN_GPIOEXP4		26
+// #define PIN_GPIOEXP4		21
 
 #define PICO_DEFAULT_UART			1
 #define PICO_DEFAULT_UART_TX_PIN	20
